001556f5426db7141660f3763b9835ea3029ddf3
mehran memarnejad <memarnejadm74@gmail.com>
2017-06-10 00:21:48 +0430

    Files added

---
+++ notes.h
@@ -0,0 +1,34 @@
+//be nam _e_ khoda
+
+
+#define sil 0
+#define sol1 1
+#define sol1_ 2
+#define la1 3
+#define la1_ 4
+#define si1 5
+#define do2 6
+#define do2_ 7
+#define re2 8
+#define re2_ 9
+#define mi2 10
+#define fa2 11
+#define fa2_ 12
+#define sol2 13
+#define sol2_ 14
+#define la2 15
+#define la2_ 16
+#define si2 17
+#define do3 18
+#define do3_ 19
+#define re3 20
+#define re3_ 21
+#define d1 1
+#define d2 2
+#define d3 3
+#define d4 4 
+#define d6 6
+#define d7 7
+#define d8 8
+#define d16 16
+#define d32 32
